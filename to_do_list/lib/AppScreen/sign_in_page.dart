3a12656421b9943bb9c22aa63c9a97debc053b2d
Hafiz M Huzaifa Khalid <66369515+huzaifa215@users.noreply.github.com>
2021-03-06 21:56:49 +0500

    radius button

--- to_do_list/lib/AppScreen/sign_in_page.dart
+++ to_do_list/lib/AppScreen/sign_in_page.dart
@@ -6,16 +6,45 @@ class SignInPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text("TO DO LIST")),
-        elevation: 20.0,// shadow under the ap bas
+        elevation: 20.0, // shadow under the ap bas
         toolbarHeight: 75,
       ),
-      body: Column(
-        children: [
-          
-
-        ],
-      ),
-
+      body: BuiltCntext(),
+      backgroundColor: Colors.grey[200],
     );
   }
+}
+
+Widget BuiltCntext() {
+  return Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          'Sign In',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 32.0,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        SizedBox(
+          height: 8.0,
+        ),
+        RaisedButton(
+          child: Text(
+            "Sign In with Google",
+            style: TextStyle(color: Colors.black87, fontSize: 15.0),
+          ),
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(5.0)),
+          ),
+          onPressed: () {},
+        ),
+      ],
+    ),
+  );
 }
