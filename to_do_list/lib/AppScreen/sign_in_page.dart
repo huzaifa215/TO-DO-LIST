799b08aef4959d17210d94d5468bc2519f9773ac
Hafiz M Huzaifa Khalid <66369515+huzaifa215@users.noreply.github.com>
2021-03-06 13:30:43 +0500

    .

---
+++ to_do_list/lib/AppScreen/sign_in_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("TO DO LIST")),
+        elevation: 20.0,// shadow under the ap bas
+        toolbarHeight: 75,
+      ),
+      body: Column(
+        children: [
+          
+
+        ],
+      ),
+
+    );
+  }
+}
