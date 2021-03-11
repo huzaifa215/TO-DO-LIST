a38aebdb3b685242034c798e175d33a420188d9b
Hafiz M Huzaifa Khalid <66369515+huzaifa215@users.noreply.github.com>
2021-03-11 21:45:22 +0500

    Future Function

--- to_do_list/lib/main.dart
+++ to_do_list/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'App_Sign_in/sign_in_page.dart';
 
-void main() async{
+Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
