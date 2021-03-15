d340f72087198320c3a0e3556927dd4b0526443a
Hafiz M Huzaifa Khalid <66369515+huzaifa215@users.noreply.github.com>
2021-03-15 20:27:52 +0500

    .

--- to_do_list/lib/App_Sign_in/SignIn/homepage.dart
+++ to_do_list/lib/App_Sign_in/SignIn/homepage.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home"),
+        title: Center(child: Text("HOME")),
+        elevation: 20.0, // shadow under the ap bas
+        toolbarHeight: 75,
       ),
     );
   }
