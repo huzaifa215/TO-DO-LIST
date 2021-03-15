00cbe241144ef374652a89ffb176817ac97117cd
Hafiz M Huzaifa Khalid <66369515+huzaifa215@users.noreply.github.com>
2021-03-15 22:07:21 +0500

    .

--- to_do_list/lib/App_Sign_in/SignIn/LandingPage/landdingpade.dart
+++ to_do_list/lib/App_Sign_in/SignIn/LandingPage/landdingpade.dart
@@ -1,11 +1,9 @@
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:to_do_list/App_Sign_in/SignIn/homepage.dart';
 import 'package:to_do_list/App_Sign_in/SignIn/sign_in_page.dart';
 
 class LanddindgPage extends StatefulWidget {
-
   @override
   _LanddindgPageState createState() => _LanddindgPageState();
 }
@@ -13,20 +11,29 @@ class LanddindgPage extends StatefulWidget {
 class _LanddindgPageState extends State<LanddindgPage> {
   // cehck user here
   User _user;
-  void _updateUser(User user){
-   setState(() {
-     _user=user;
-   });
+
+  @override
+  void _updateUser(User user) {
+    setState(() {
+      _user = user;
+    });
   }
+
+  // check the user login or not
+  void initState() {
+    super.initState();
+    _updateUser(FirebaseAuth.instance.currentUser);
+  }
+
   @override
   Widget build(BuildContext context) {
-    if(_user==null) {
+    if (_user == null) {
       return SignInPage(
-        onSignIn:_updateUser,
+        onSignIn: _updateUser,
       );
     }
     return HomePage(
-      OnSignOut: ()=> _updateUser(null),
-    );// Temporary placeholder for homepage
+      OnSignOut: () => _updateUser(null),
+    ); // Temporary placeholder for homepage
   }
 }
