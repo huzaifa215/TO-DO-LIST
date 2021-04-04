483d1a3f4ec07a3d75c13673a3b28b0a1ceaed25
Hafiz M Huzaifa Khalid <66369515+huzaifa215@users.noreply.github.com>
2021-04-04 19:25:54 +0500

    have to done the logic

--- to_do_list/lib/App_Sign_in/SignIn/email_sign_in_form.dart
+++ to_do_list/lib/App_Sign_in/SignIn/email_sign_in_form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_list/Common_widgets/form_submit_button.dart';
 import 'package:to_do_list/Services/Auth.dart';
+import 'package:email_auth/email_auth.dart';
 
 enum EmailSignInFormType { signIn, register }
 
@@ -16,14 +17,20 @@ class EmailSignInForm extends StatefulWidget {
 class _EmailSignInFormState extends State<EmailSignInForm> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final FocusNode _emailFocusNode=FocusNode();
-  final FocusNode _passwordFocusNode=FocusNode();
+  final TextEditingController _OTPController = TextEditingController();
+
+  final FocusNode _emailFocusNode = FocusNode();
+  final FocusNode _passwordFocusNode = FocusNode();
+  final FocusNode _OTPFocusNode = FocusNode();
 
   EmailSignInFormType _formType = EmailSignInFormType.signIn;
 
-  String get _email => _emailController.text;
 
+  String get _email => _emailController.text;
   String get _password => _passwordController.text;
+  String get _OTP => _OTPController.text;
+
+
 
   void _submit() async {
     // TODO: print the email and password but than transfer data to the firebase
@@ -33,14 +40,20 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
       } else {
         await widget.auth.CreateUserWithEmailAndPassword(_email, _password);
       }
-      Navigator.of(context).pop();// move to landing page
+      Navigator.of(context).pop(); // move to landing page
     } catch (e) {
       print(e.toString());
     }
   }
 
-  void _emailEditingComplete(){
-    
+  void _emailEditingComplete() {
+    // TODO: keep track on the next widget or the widgets to follow the flow
+    FocusScope.of(context).requestFocus(_passwordFocusNode);
+  }
+
+  void _passwordEditingComplete() {
+    // TODO: keep track on the next widget or the widgets to follow the flow
+    FocusScope.of(context).requestFocus(_OTPFocusNode);
   }
 
 // TODO: Toggole set the _formkey here and when the _builtChilder will call it changes
@@ -54,41 +67,60 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
     _passwordController.clear();
   }
 
+  //TODO:sending the OTP message on the email
+  void _sendOTP() async {
+    EmailAuth.sessionName = "test Session";
+    var res = await EmailAuth.sendOtp(receiverMail: _email);
+    if (res) {
+      print("OTP Send");
+    }
+    else{
+      print("Problem in sending OTP");
+    }
+  }
+
+  //TODO:Verify the OTP that is send on the email on yours
+  void _verifyOTP() async {
+    var res = EmailAuth.validate(receiverMail: _email, userOTP: _OTP);
+    if (res) {
+      print("OPT Verify");
+      _submit();
+    }
+    else{
+      print("invalid OTP");
+    }
+  }
+
+// TextField _showOTPtextFeild(){
+//     if(_formType !=EmailSignInFormType.signIn){
+//       return ;
+//     }
+//     return _buildOTP()
+//
+// }
+
+
+
   List<Widget> _builtChildern() {
     final primarytext = (_formType == EmailSignInFormType.signIn
-        ? "Sign In"
-        : "Create an Account");
+        ? " Sign In"
+        : "Verify OTP");
     final secondarytext = (_formType == EmailSignInFormType.signIn
         ? "Need an Account? Register"
         : "Have an Account? Sign In");
     return [
-      // email feild input
-      TextField(
-        // TODO: what ever will be written in the textfield it aotumaticcaly edited in the controller
-        controller: _emailController,
-        focusNode: _emailFocusNode,
-        decoration: InputDecoration(
-          labelText: 'Email',
-          hintText: "test@test.com",
-        ),
-        //autocorrect: false,//TODO: used to enable the keyboard suggestion
-        keyboardType: TextInputType.emailAddress,//TODO: Show that keyboard that is suitable for emails
-        textInputAction: TextInputAction.next,// TODO: show the button that on which we click move to the password section
-        onEditingComplete: _emailEditingComplete,
-      ),
+      // email field input
+      _buildemail(),
       SizedBox(
         height: 8.0,
       ),
-      TextField(
-        controller: _passwordController,
-        decoration: InputDecoration(
-          labelText: 'Password',
-        ),
-        obscureText: true,
-        autocorrect: false,
-        keyboardType: TextInputType.visiblePassword,
-        textInputAction: TextInputAction.done,
+      // password field input
+      _buildpassword(),
+      SizedBox(
+        height: 8.0,
       ),
+      // OTP text feild
+      _buildOTP(),
       SizedBox(
         height: 8.0,
       ),
@@ -96,6 +128,7 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
         text: primarytext,
         onpressed: _submit,
       ),
+
       SizedBox(
         height: 8.0,
       ),
@@ -118,5 +151,55 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
       ),
     );
   }
-}
 
+  TextField _buildemail() {
+    return TextField(
+      // TODO: what ever will be written in the textfield it aotumaticcaly edited in the controller
+      controller: _emailController,
+      focusNode: _emailFocusNode,
+      decoration: InputDecoration(
+        labelText: 'Email',
+        hintText: "test@test.com",
+        //TODO: written as a power on the email
+        suffixIcon:TextButton(
+          child: Text("Send OTP"),
+          onPressed : _sendOTP,
+        )
+      ),
+      //autocorrect: false,//TODO: used to enable the keyboard suggestion
+      keyboardType: TextInputType.emailAddress,
+      //TODO: Show that keyboard that is suitable for emails
+      textInputAction: TextInputAction.next,
+      // TODO: show the button that on which we click move to the password section
+      onEditingComplete: _emailEditingComplete,
+    );
+  }
+
+  TextField _buildpassword() {
+    return TextField(
+      controller: _passwordController,
+      focusNode: _passwordFocusNode,
+      onEditingComplete: _passwordEditingComplete,
+      decoration: InputDecoration(
+        labelText: 'Password',
+      ),
+      obscureText: true,
+      autocorrect: false,
+      keyboardType: TextInputType.visiblePassword,
+      textInputAction: TextInputAction.next,
+    );
+  }
+
+  TextField _buildOTP() {
+    return TextField(
+      controller: _OTPController,
+      focusNode: _OTPFocusNode,
+      onEditingComplete: _verifyOTP,
+      decoration:
+          InputDecoration(labelText: 'OTP Code', hintText: "Enter the OTP"),
+      autocorrect: false,
+      keyboardType: TextInputType.visiblePassword,
+      textInputAction: TextInputAction.done,
+    );
+  }
+}
